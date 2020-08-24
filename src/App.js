97fc6ad640e4eb19ae8b542ce944ce159b45ec3b
Georgedev2 <nwobodogeorge98@gmail.com>
2020-08-24 08:24:17 -0700

    my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./unplash_api/Search";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
